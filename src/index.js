a1137a5939a9e278bcb22d44113b6ca3c083bd61
Ruijie <yinyppyin@gmail.com>
2019-04-18 10:32:10 -0400

    patch

--- src/index.js
+++ src/index.js
@@ -4,18 +4,12 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './store_redux/index';
 import ErrorBoundary from './hoc/ErrorBoundary/ErrorBoundary';
-
 import thunk from 'redux-thunk';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 ReactDOM.render(
   <ErrorBoundary>
     <Provider store={store}>
